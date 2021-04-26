36cc7d4e95177a8b70d207e774af4c2e3e17d845
adambailey <arb5433@gmail.com>
2021-04-26 16:02:21 -0400

    “clean”

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -30,12 +30,6 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
-        {/* <Route path="/login" exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm /> */}
-        {/* </Route> */}
         <ProtectedRoute path="/users" exact={true} >
           <UsersList/>
         </ProtectedRoute>
