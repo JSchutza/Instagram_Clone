80db15c4024c5dd14b80a39d2bcc09753e5ed86b
adambailey <arb5433@gmail.com>
2021-04-26 16:02:43 -0400

    “clean”

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch} from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LoginForm from "./components/auth/LoginForm";
-import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
-// import { authenticate } from "./services/auth";
+import { authenticate } from "./services/auth";
 import { authenticate } from "./store/session";
 
 function App() {
-  // const [authenticated, setAuthenticated] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
   const dispatch = useDispatch()
   const [loaded, setLoaded] = useState(false);
 
