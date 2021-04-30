7409a9e83e2658ee23336af45337e5d351a7919a
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-04-30 13:30:41 -0500

    save

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import SingupForm from './SignupForm.js';
 
+import styles from './signupform.module.css'
+
 function SignupFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className='signup-btn' onClick={() => setShowModal(true)}>Sign-Up</button>
+      <button className={styles.signup_btn} onClick={() => setShowModal(true)}>Sign-Up</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <SingupForm />
