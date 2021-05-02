da7885784745270a2444a0abcfb5399950587414
Lucian Jones <lucianjones96@gmail.com>
2021-05-01 21:52:06 -0400

    “fun stuff”

--- react-app/src/components/CreatePost/CreatePost.js
+++ react-app/src/components/CreatePost/CreatePost.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import "./createpost.css";
 
 const CreatePost = ({ setShowModal, icon }) => {
   const [captionText, setCaptionText] = useState("");
   const [img, setImg] = useState("");
   const [imageLoading, setImageLoading] = useState(false);
   const [errors, setErrors] = useState([]);
+  const [url, setUrl] = useState("");
   const history = useHistory();
 
   useEffect(() => {
@@ -17,10 +19,6 @@ const CreatePost = ({ setShowModal, icon }) => {
     setErrors(errors);
   }, [img]);
 
-
-
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
@@ -29,61 +27,70 @@ const CreatePost = ({ setShowModal, icon }) => {
 
     setImageLoading(true);
 
-    const res = await fetch('/api/posts', {
-        method: "POST",
-        body: formData,
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      body: formData,
     });
     if (res.ok) {
       await res.json();
       setImageLoading(false);
-      setShowModal(false)
+      setShowModal(false);
       history.push("/images");
-    }
-    else {
+    } else {
       setImageLoading(false);
       console.log("error");
     }
-    history.push('/profile')
-  }
+    history.push("/profile");
+  };
 
   const updateImage = (e) => {
     const file = e.target.files[0];
     setImg(file);
-  }
+    setUrl(URL.createObjectURL(file));
+  };
+
+  const change = () => {
+    setImg("");
+    setUrl("");
+  };
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-      >
-        <label>
-          Caption
+      <form className="create-post-form" onSubmit={handleSubmit}>
+        {!img && (
+        <>
+          <label className="create-post-label" for='file'>Upload Image</label>
+          <input id='file' className="create-post-file-input" type="file" accept="image/*" onChange={updateImage} />
+        </>
+        )}
+        {img && (
+          <>
+            <img className="create-post-img" alt="upload" src={url}></img>
+            {!imageLoading && <button onClick={change} className="create-post-change">
+              Change Image
+            </button>}
+          </>
+        )}
+        {!imageLoading && <div className="caption-div">
+          <label>Caption</label>
           <input
+            className="create-post-caption"
             type="text"
             name="caption"
             value={captionText}
             onChange={(e) => setCaptionText(e.target.value)}
           />
-        </label>
+          <button
+            type="submit"
+            styles={{ backgroundImage: `url(${icon})` }}
+            disabled={!!errors.length}
+          >
+            {" "}
+            Create Post{" "}
+          </button>
+        </div>}
 
-        <label>
-          Image
-          <input
-              type="file"
-              accept="image/*"
-              onChange={updateImage}
-            />
-        </label>
-
-        <button
-          type="submit"
-          styles={{ backgroundImage: `url(${icon})` }}
-          disabled={!!errors.length}
-        >
-          {" "}
-          Create Post{" "}
-        </button>
-        {(imageLoading)&& <p>Loading...</p>}
+        {imageLoading && <p>Loading...</p>}
       </form>
     </>
   );
